8278f9f2f080ca5eeb54b81b1b5001b64d26dbc2
System <none>
2017-10-10 07:05:44 +0000

    🕹🏁 Updated with Glitch

--- server.js
+++ server.js
@@ -4,21 +4,49 @@
 // init project
 var express = require('express');
 var app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var db_url = "mongodb://sathvikkatam:Unicorn_123@ds113435.mlab.com:13435/urlshort";
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-app.get("/*" , function(req, res){
+app.get("/short-it/*" , function(req, res){
   var requested_url = req.params[0];
+  connectDb(requested_url);
+  var response_body = {
+    "long_url": requested_url
+  }
   
-  res.send("OK");
+  res.send(response_body);
 });
 
 
-app.get("/" , function(req, res){
-  res.send("ADD URL");
+function connectDb(requested_url){
+  MongoClient.connect(db_url, function(err, db) {
+  if (err) throw err;
+  var myobj = { url: requested_url};
+  db.collection("url_list").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted "+res._id);
+    db.close();
+  });
+});
+}
+
+app.get("/:endcoded_id" , function(req, res){
+ var url_id = req.params.endcoded_id;
+  var url_redirect;
+  var query = {_id : url_id }
+  MongoClient.connect(db_url, function(err, db) {
+  if (err) throw err;
+  db.collection("url_list").find(query).toArray(function(err, result) {
+    if (err) throw err;
+        url_redirect = result;
+          db.close();
+        });
+    });
+  res.redirect(url_redirect);
 });
 
 
